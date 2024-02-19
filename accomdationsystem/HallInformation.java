455e445548ccbdf913ed76ebccfdc015b387d10d
Veenashaji1 <116875239+Veenashaji1@users.noreply.github.com>
2024-02-19 13:22:04 +0000

    Add files via upload

---
+++ accomdationsystem/HallInformation.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accomdationsystem;
+
+/**
+ *
+ * @author simla
+ */
+public class HallInformation {
+    private String hallName;
+    private String hallManagerName;
+    private int totalRooms;
+    private int availableRooms;
+    private int roomsNeedingCleaning;
+    private int offlineRooms;
+
+    // Constructor
+    public HallInformation(String hallName, String hallManagerName, int totalRooms, int availableRooms,
+                           int roomsNeedingCleaning, int offlineRooms) {
+        this.hallName = hallName;
+        this.hallManagerName = hallManagerName;
+        this.totalRooms = totalRooms;
+        this.availableRooms = availableRooms;
+        this.roomsNeedingCleaning = roomsNeedingCleaning;
+        this.offlineRooms = offlineRooms;
+    }
+
+    // Getter methods
+    public String getHallName() {
+        return hallName;
+    }
+
+    public String getHallManagerName() {
+        return hallManagerName;
+    }
+ 
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public int getAvailableRooms() {
+        return availableRooms;
+    }
+
+    public int getRoomsNeedingCleaning() {
+        return roomsNeedingCleaning;
+    }
+
+    public int getOfflineRooms() {
+        return offlineRooms; 
+    }
+} 
---
+++ accomdationsystem/RoomCleaningStatus.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accomdationsystem;
+
+/**
+ *
+ * @author simla
+ */
+ public enum RoomCleaningStatus {
+    CLEAN,
+    DIRTY,
+    OFFLINE
+}
